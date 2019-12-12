4afc9c544ac7f99e3fc942c543e9254195dbd975
Erick Augusto Morais Fauro <erick.fauro@ENG-1721.maxmilhas.local>
2019-12-12 14:50:44 -0300

    Fiz o 2º exercicio

---
+++ exercicio2.js
@@ -0,0 +1,5 @@
+// Como pedir ao usuario que insira algo no prompt
+//FEITO
+var um = parseInt(window.prompt('Digite o primeiro valor:', 'Digite aqui!'));
+var dois = parseInt(window.prompt('Digite o segundo valor:', 'Digite aqui!'));
+var tres = parseInt(window.prompt('Digite o terceiro valor:', 'Digite aqui!'));
