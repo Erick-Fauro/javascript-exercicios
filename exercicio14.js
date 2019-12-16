144d302c1be214f7846c2a286cda8a79d2b40691
Erick Augusto Morais Fauro <erick.fauro@ENG-1721.maxmilhas.local>
2019-12-16 13:23:29 -0300

    Fiz o 14º exercicio

---
+++ exercicio14.js
@@ -0,0 +1,11 @@
+
+/*
+14 – Faça um programa onde o usuário possa escolher qual a tabuada que se deseja imprimir. Exiba a tabuada desse número de 1 a 10.
+*/
+
+let tabuada = 10;
+for (x = 0; x <= 10; x++) {
+
+    console.log("Tabuada do: " + tabuada)
+    console.log(x + "x" + tabuada + "=" + x * tabuada);
+}
