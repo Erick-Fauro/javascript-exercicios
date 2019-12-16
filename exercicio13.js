390d708f470d2cff049f3378e402a47c73a22423
Erick Augusto Morais Fauro <erick.fauro@ENG-1721.maxmilhas.local>
2019-12-16 13:22:14 -0300

    Fiz o 13º exercicio

---
+++ exercicio13.js
@@ -0,0 +1,14 @@
+
+/*
+13 – Faça um programa que calcule a soma dos primeiros 50 números pares.
+*/
+
+let numeros;
+for (numeros = 0; numeros <= 50; numeros++) {
+    if (numeros % 2 == 0) {
+
+        console.log(`O número ${numeros} é par, pois o resto da divisão por dois é igual a zero`)
+
+    }
+
+}
